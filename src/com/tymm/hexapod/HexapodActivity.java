45b4ea8a8f17c667944dd1499d1d586be41ddc7b
Timm <tymmm1@gmail.com>
2014-04-02 23:57:41 +0200

    Initial commit

---
+++ src/com/tymm/hexapod/HexapodActivity.java
@@ -0,0 +1,13 @@
+package com.tymm.hexapod;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HexapodActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
