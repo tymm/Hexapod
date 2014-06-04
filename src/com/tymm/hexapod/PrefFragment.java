a0bfb35a4a281609fdb8b5e145ab5dcdb3fdb4e1
Timm <tymmm1@gmail.com>
2014-06-04 22:28:00 +0200

    Adding PreferenceActivity for settings regarding the hexapod

---
+++ src/com/tymm/hexapod/PrefFragment.java
@@ -0,0 +1,12 @@
+package com.tymm.hexapod;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class PrefFragment extends PreferenceFragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+}
---
+++ src/com/tymm/hexapod/PreferenceActivity.java
@@ -0,0 +1,19 @@
+package com.tymm.hexapod;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+public class PreferenceActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		PrefFragment prefFragment = new PrefFragment();
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		fragmentTransaction.replace(android.R.id.content, prefFragment);
+		fragmentTransaction.commit();
+	}
+}
