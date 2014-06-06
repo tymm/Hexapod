b9524be18a50db50d668b4e6c60f869a3520fb24
Timm <tymmm1@gmail.com>
2014-06-06 21:11:44 +0200

    Using degrees instead of radian and only send when the difference between the previos value and the new value is big enough

--- src/com/tymm/hexapod/SensorInformation.java
+++ src/com/tymm/hexapod/SensorInformation.java
@@ -5,16 +5,26 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 import android.hardware.Sensor;
+import java.lang.Math;
 
 class SensorInformation implements SensorEventListener {
 	private Sensor mRotationVectorSensor;
 	private final float[] mRotationMatrix = new float[16];
 	private SensorManager mSensorManager;
+	private float[] orientation = new float[3];
+	private float[] prev_orientation = new float[3];
+	private final int diff_degree = 5;
 
 	public SensorInformation(SensorManager SensorManager) {
 		mSensorManager = SensorManager;
 
+		// find the rotation-vector sensor
 		mRotationVectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+		mRotationMatrix[ 0] = 1;
+		mRotationMatrix[ 4] = 1;
+		mRotationMatrix[ 8] = 1;
+		mRotationMatrix[12] = 1;
 	}
 
 	public void start() {
@@ -32,15 +42,31 @@ class SensorInformation implements SensorEventListener {
 		// we received a sensor event. it is a good practice to check
 		// that we received the proper event
 		if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-			Log.i("Hexapod", "Rotation changed:");
+			SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
+			SensorManager.getOrientation(mRotationMatrix, orientation);
 
-			Float x = event.values[0];
-			Float y = event.values[1];
-			Float z = event.values[2];
+			if (isDiffBigEnoughChange(orientation)) {
+				Log.i("Hexapod", "Z: " + Math.toDegrees(orientation[0]) + " X: " + Math.toDegrees(orientation[1]) + " Y: " + Math.toDegrees(orientation[2]));
+				// Sending sensor information here
+			}
+		}
+	}
 
-			Log.i("Hexapod", x.toString());
-			Log.i("Hexapod", y.toString());
-			Log.i("Hexapod", z.toString());
+	public boolean isDiffBigEnoughChange(float[] orientation) {
+		double Z_prev = Math.toDegrees(prev_orientation[0]);
+		double X_prev = Math.toDegrees(prev_orientation[1]);
+		double Y_prev = Math.toDegrees(prev_orientation[2]);
+
+		double Z = Math.toDegrees(orientation[0]);
+		double X = Math.toDegrees(orientation[1]);
+		double Y = Math.toDegrees(orientation[2]);
+
+		if (Math.abs(Z-Z_prev) >= diff_degree || Math.abs(X-X_prev) >= diff_degree || Math.abs(Y-Y_prev) >= diff_degree) {
+			// Copy by value
+			System.arraycopy(orientation, 0, this.prev_orientation, 0, orientation.length);
+			return true;
+		} else {
+			return false;
 		}
 	}
 
