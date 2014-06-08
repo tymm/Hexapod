7d3d5ba55cb88f75e3cc0e5d1c55a04da2bc8ed0
Timm <tymmm1@gmail.com>
2014-06-09 01:15:18 +0200

    Added 3d animation which shows the rotation

--- src/com/tymm/hexapod/RotationFragment.java
+++ src/com/tymm/hexapod/RotationFragment.java
@@ -7,11 +7,13 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.opengl.GLSurfaceView;
 
 public class RotationFragment extends Fragment {
 	// Sensor Manager
 	private SensorManager mSensorManager;
 	private SensorInformation sensor;
+	private GLSurfaceView mGLSurfaceView;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -26,8 +28,10 @@ public class RotationFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_rotation, container, false);
-		return rootView;
+		mGLSurfaceView = new GLSurfaceView(this.getActivity());
+		mGLSurfaceView.setRenderer(sensor);
+
+		return mGLSurfaceView;
 	}
 
 	@Override
@@ -43,11 +47,13 @@ public class RotationFragment extends Fragment {
 				sensor.stop();
 			} catch (NullPointerException e) {}
 		}
+		mGLSurfaceView.onResume();
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
 		sensor.stop();
+		mGLSurfaceView.onPause();
 	}
 }
--- src/com/tymm/hexapod/SensorInformation.java
+++ src/com/tymm/hexapod/SensorInformation.java
@@ -1,14 +1,17 @@
 package com.tymm.hexapod;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
 import android.util.Log;
-import android.hardware.Sensor;
-import java.lang.Math;
-import android.content.Context;
 
-class SensorInformation implements SensorEventListener {
+class SensorInformation implements GLSurfaceView.Renderer, SensorEventListener {
 	private Sensor mRotationVectorSensor;
 	private final float[] mRotationMatrix = new float[16];
 	private SensorManager mSensorManager;
@@ -17,11 +20,15 @@ class SensorInformation implements SensorEventListener {
 	private final int diff_degree = 5;
 	private Communication comm;
 
+	private Cube mCube;
+
 	public SensorInformation(SensorManager SensorManager, Context context) {
 		mSensorManager = SensorManager;
 
 		// find the rotation-vector sensor
 		mRotationVectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+		mCube = new Cube();
 
 		mRotationMatrix[ 0] = 1;
 		mRotationMatrix[ 4] = 1;
@@ -31,19 +38,16 @@ class SensorInformation implements SensorEventListener {
 		// Get bluetooth communication object
 		comm = ((Communication)context.getApplicationContext());
 
-		Log.i("Fragment", "Created object");
 	}
 
 	public void start() {
 		// enable our sensor when the activity is resumed, ask for
 		// 10 ms updates.
-		Log.i("Fragment", "Started listening");
 		mSensorManager.registerListener(this, mRotationVectorSensor, 10000);
 	}
 
 	public void stop() {
 		// make sure to turn our sensor off when the activity is paused
-		Log.i("Fragment", "Stoped listening");
 		mSensorManager.unregisterListener(this);
 	}
 
@@ -97,5 +101,36 @@ class SensorInformation implements SensorEventListener {
 	}
 
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	}
+
+	public void onDrawFrame(GL10 gl) {
+		// clear screen
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		// set-up modelview matrix
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		gl.glTranslatef(0, 0, -3.0f);
+		gl.glMultMatrixf(mRotationMatrix, 0);
+		// draw our object
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+		mCube.draw(gl);
+	}
+
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// set view-port
+		gl.glViewport(0, 0, width, height);
+		// set projection matrix
+		float ratio = (float) width / height;
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
+		gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+	}
+
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// dither is enabled by default, we don't need it
+		gl.glDisable(GL10.GL_DITHER);
+		// clear screen in white
+		gl.glClearColor(1,1,1,1);
 	}
 }
