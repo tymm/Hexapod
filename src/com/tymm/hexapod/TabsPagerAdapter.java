072148a2c3a1ba0a4381d1e1bdf49c3b0c4d0507
Timm <tymmm1@gmail.com>
2014-06-09 17:52:26 +0200

    Don't create a new Fragment object. Instead return the original one

--- src/com/tymm/hexapod/TabsPagerAdapter.java
+++ src/com/tymm/hexapod/TabsPagerAdapter.java
@@ -3,38 +3,30 @@ package com.tymm.hexapod;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v13.app.FragmentPagerAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TabsPagerAdapter extends FragmentPagerAdapter {
+	private List<Fragment> fragments;
 
 	public TabsPagerAdapter(FragmentManager fm) {
 		super(fm);
+		this.fragments = new ArrayList<Fragment>();
+		fragments.add(new ControlPadFragment());
+		fragments.add(new ControlFragment());
+		fragments.add(new RotationFragment());
+		fragments.add(new PrefFragment());
 	}
 
 	@Override
 	public Fragment getItem(int index) {
-
-		switch (index) {
-			case 0:
-				// ControlPad fragment activity
-				return new ControlPadFragment();
-			case 1:
-				// Control fragment activity
-				return new ControlFragment();
-			case 2:
-				// Rotation fragment activity
-				return new RotationFragment();
-			case 3:
-				// Settings fragment activity
-				return new PrefFragment();
-		}
-
-		return null;
+		return fragments.get(index);
 	}
 
 	@Override
 	public int getCount() {
 		// get item count - equal to number of tabs
-		return 4;
+		return fragments.size();
 	}
 
 }
